671ea5bd6601967848785434527220dda0481a30
Julius Busecke <jbusecke@princeton.edu>
2021-05-06 01:16:58 -0400

    added test scripts

---
+++ scripts/download_zenodo_files.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Create filelist !!!YOU HAVE TO CHANGE YOUR Repository ID
+python scripts/parse_filenames.py
+
+#download the files in the list
+cd data
+wget -i ../zenodo_filelist.txt
+
+#delete the filelist
+rm ../zenodo_filelist.txt
