d35c12afc6652d717385f41a39eb6db396372934
Julius Busecke <jbusecke@princeton.edu>
2021-05-06 18:49:29 -0400

    update download script with untar

--- scripts/download_zenodo_files.sh
+++ scripts/download_zenodo_files.sh
@@ -4,8 +4,10 @@
 python scripts/parse_filenames.py
 
 #download the files in the list
-cd data/test
+cd data
 wget -i ../../zenodo_filelist.txt
 
 #delete the filelist
-# rm ../../zenodo_filelist.txt
+rm ../../zenodo_filelist.txt
+
+tar -xf busecke_etal_2021_aguadv.tar
