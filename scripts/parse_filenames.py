bbfa218dc0cbf6ac81c402e3d6bdb8b2d5be08c4
Julius Busecke <jbusecke@jupyter.rc.princeton.edu>
2022-03-16 01:20:47 -0400

    added missing files to collect folder

--- scripts/parse_filenames.py
+++ scripts/parse_filenames.py
@@ -1,14 +1,14 @@
 import requests
 
 # you need to modify this to your zenodo ID
-zenodo_id = 2648592 # old grl data
+# zenodo_id = 2648592 # old grl data
 # zenodo_id = 4739876 # new files...which dont work yet...
 
 # r = requests.get("https://zenodo.org/api/records/%i" % zenodo_id)
 
 # sandbox version
 # zenodo_id = 952702
-zenodo_id = 1035179 #second revision edition
+zenodo_id = 1035282 #second revision edition
 r = requests.get("https://sandbox.zenodo.org/api/records/%i" % zenodo_id)
 
 file = open("zenodo_filelist.txt", "w")
